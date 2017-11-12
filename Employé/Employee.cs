cdfc2d1a0f028d367b71d145e444b961cad5fa9e
Byakuei <spartan117di@hotmail.com>
2017-11-12 17:42:11 +0100

    Modifying files's name, added Company.cs and Employee.cs

---
+++ Employé/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employé
+{
+    class Employee
+    {
+        protected String name;
+        protected DateTime recruitmentDate;
+        protected int age;
+
+        public Employee(String name, int age, int year, int month, int day)
+        {
+            this.name = name;
+            this.age = age;
+            this.recruitmentDate = new DateTime(year, month, day);
+        }
+
+    }
+}
